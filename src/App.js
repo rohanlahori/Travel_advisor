683fdc04e34c48a06dd639a77f193d8bf954cf81
Rohan Lahori <rohanlahori99@gmail.com>
2022-11-04 12:20:05 +0530

    Structured

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import Header  from "./component/Header/Header";
+import Map from "./component/Map/Map"
+import List from "./component/List/List"
+import {CssBaseline,Grid} from '@mui/material';
+
 
 const App=()=>{
     return(
-        <div>
-            <h2>Is it working</h2>
-            <h1>Hello World</h1>
-        </div>
+        <>
+            <CssBaseline/>
+            <Header/>
+            <Grid container spacing={3} style={{width:'100%'}}>
+            <Grid item xs={12} md={4}>
+                <List/>
+            </Grid>
+
+            <Grid item xs={12} md={8}>
+                <Map/>
+            </Grid>
+            </Grid>  
+        </>
     );
 }
 
