601490d64ba824b430567997594da146c2034cd6
Rohan Lahori <rohanlahori99@gmail.com>
2022-11-04 11:16:45 +0530

    Initialization

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App=()=>{
+    return(
+        <div>
+            <h2>Is it working</h2>
+            <h1>Hello World</h1>
+        </div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
-import { ReactDOM } from "react";
-
+import ReactDOM from "react-dom"
 import App from './App'
+import {createRoot} from 'react-dom/client';
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+
+const rootElement=document.getElementById('root');
+const root=createRoot(rootElement)
+
+root.render(
+    <App />
+)
