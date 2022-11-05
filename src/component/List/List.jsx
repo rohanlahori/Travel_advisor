38cd282038d83bc7225367efaed4ca97981f3ee0
Rohan Lahori <rohanlahori99@gmail.com>
2022-11-05 17:17:32 +0530

    commit

--- src/component/List/List.jsx
+++ src/component/List/List.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import NativeSelect from '@mui/material/NativeSelect';
 
-const List=()=>{
-    return(
-        <h1>List</h1>
-    )
+export default function NativeSelectDemo() {
+  return (
+    <Box sx={{ minWidth: 120 }}>
+        <h1>
+            Restaurant Hotel & Attraction around 
+                You.
+        </h1>
+      <FormControl fullWidth>
+        <InputLabel variant="standard" htmlFor="uncontrolled-native">
+          Age
+        </InputLabel>
+        <NativeSelect
+          defaultValue={30}
+          inputProps={{
+            name: 'age',
+            id: 'uncontrolled-native',
+          }}
+        >
+          <option value={10}>Ten</option>
+          <option value={20}>Twenty</option>
+          <option value={30}>Thirty</option>
+        </NativeSelect>
+      </FormControl>
+    </Box>
+  );
 }
-export default List
