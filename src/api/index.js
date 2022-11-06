398bb02165ced7cde7e7eefd0e8ceddf3616a693
Rohan Lahori <rohanlahori99@gmail.com>
2022-11-06 12:06:52 +0530

    template function

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,11 @@
 import axios from "axios"
+
+const getPlacesDate=()=>{
+    try{
+        // const response =await axios.get('');
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+}
